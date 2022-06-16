d28d804f466f29fdb3e5d742a69505774883ebcc
Mario242619 <107264693+Mario242619@users.noreply.github.com>
2022-06-15 23:34:16 -0400

    Add files via upload

--- __main__.py
+++ __main__.py
@@ -5,7 +5,7 @@ import pulumi_aws as aws
 vpc = aws.ec2.get_vpc(default=True)
 
 group = aws.ec2.SecurityGroup(
-    "Se-app",
+    "Secure-app",
     description="anything man",
     ingress=[
         {
@@ -41,7 +41,7 @@ instance = aws.ec2.Instance(
 
 #deployer = aws.ec2.KeyPair("deployer",
     #key_name="acer4",
-    #public_key="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6tyPEFEzV0LX3X8BsXdMsQz1x2cEikKDEY0aIj41qgxMCP/iteneqXSIFZBp5vizPvaoIR3Um9xK7PGoW8giupGn+EPuxIA4cDM4vzOqOkiMPhz5XK0whEjkVzTo4+S0puvDZuwIsdiW9mxhJc7tgBNL0cYlWSYVkz4G/fslNfRPW5mYAM49f4fhtxPb5ok4Q2Lg9dPKVHO/Bgeu5woMc7RY0p1ej6D4CKFE6lymSDJpW0YHX/wqE9+cfEauh7xZcG0q9t2ta6F6fmX0agvpFyZo8aFbXeUBr7osSCJNgvavWbM/06niWrOvYX2xwWdhXmXSrbX8ZbabVohBK41 email@example.com",
+    #public_key=",
     #opts=pulumi.ResourceOptions(protect=False)
                            #)
 
